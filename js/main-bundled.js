4a6f1d017e9061a6219054291576c9f1e430b63f
javadev16 <65906940+javadev16@users.noreply.github.com>
2020-08-28 16:03:22 -0500

    Update main-bundled.js

--- js/main-bundled.js
+++ js/main-bundled.js
@@ -428,7 +428,7 @@ module.exports = function(birthYear) {
 var myTemplate = __webpack_require__(3);
 
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/pets-data.json');
+ourRequest.open('GET', 'https://javadev16.github.io/json-example/pets-data.json');
 ourRequest.onload = function() {
   if (ourRequest.status >= 200 && ourRequest.status < 400) {
     var data = JSON.parse(ourRequest.responseText);
